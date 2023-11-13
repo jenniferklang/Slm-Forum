16c1d3405b8ea5f557a798f68b4f9adbc461e22b
Johan Hermansson <karljohan.hermanssons@gmail.com>
2023-11-13 11:16:44 +0100

    first commit

---
+++ backend/index.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const dotenv = require('dotenv');
+const { Client } = require('pg');
+
+dotenv.config();
+
+const client = new Client({
+  connectionString: process.env.PGURI,
+});
+client.connect();
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/api', async (_request, response) => {
+  console.log('Get request');
+  const { rows } = await client.query(
+    'SELECT * FROM cities ORDER BY name ASC',
+    []
+  );
+
+  response.send(rows);
+});
+
+app.put('/api', async (request, response) => {
+  console.log('Put request');
+  const { id, population } = request.body;
+
+  await client.query('UPDATE cities SET population = $1 WHERE id = $2', [
+    population,
+    id,
+  ]);
+
+  const { rows } = await client.query(
+    'SELECT * FROM cities ORDER BY name ASC',
+    []
+  );
+
+  response.send(rows);
+});
+
+app.delete('/api', async (request, response) => {
+  console.log('Delete request');
+  const { id } = request.body;
+
+  await client.query('DELETE FROM cities WHERE id = $1', [id]);
+
+  const { rows } = await client.query(
+    'SELECT * FROM cities ORDER BY name ASC',
+    []
+  );
+
+  response.send(rows);
+});
+
+app.post('/api', async (request, response) => {
+  console.log('Post request');
+  const { name, population } = request.body;
+
+  try {
+    await client.query(
+      'INSERT INTO cities (name, population) VALUES ($1, $2)',
+      [name, population]
+    );
+
+    const { rows } = await client.query(
+      'SELECT * FROM cities ORDER BY name ASC',
+      []
+    );
+
+    response.send(rows);
+  } catch (error) {
+    console.log(error);
+
+    if (error.code === '23505') {
+      return response.status(400).send('City already exists');
+    }
+  }
+});
+
+// app.use(express.static(path.join(path.resolve(), 'dist')));
+
+app.listen(port, () => {
+  console.log(`Redo på ${port}`);
+});
---
+++ frontend/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <main>
+    <h1 class="text-h2 mb-12 text-center">Software Lifecycle Management</h1>
+    <CitiesList :receivedCities="cities" class="mb-12" />
+    <v-expansion-panels style="max-width: 600px" class="mx-auto">
+      <v-expansion-panel>
+        <v-expansion-panel-title>
+          <h3 class="text-h5">Lägg till stad</h3>
+        </v-expansion-panel-title>
+        <v-expansion-panel-text>
+          <AddCityForm @city-added="onCityAdded" />
+        </v-expansion-panel-text>
+      </v-expansion-panel>
+    </v-expansion-panels>
+  </main>
+</template>
+
+<script>
+import CitiesList from './components/CitiesList.vue';
+import AddCityForm from './components/AddCityForm.vue';
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    cities: [],
+  }),
+
+  watch: {
+    cities: {
+      handler() {
+        console.log('cities changed');
+        console.log(this.cities);
+      },
+      deep: true,
+    },
+  },
+
+  methods: {
+    onCityAdded(cities) {
+      this.cities = cities;
+    },
+  },
+
+  components: {
+    CitiesList,
+    AddCityForm,
+  },
+};
+</script>
