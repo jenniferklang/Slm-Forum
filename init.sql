67fde0e2deb25c85cde3d5ffd1e1341a836ee219
Johan Hermansson <karljohan.hermanssons@gmail.com>
2023-11-14 10:11:41 +0100

    Test

--- init.sql
+++ init.sql
@@ -1,44 +1,88 @@
--- Skapar tabellen cities om den inte redan finns i databasen.
-CREATE TABLE IF NOT EXISTS cities (
-  id serial PRIMARY KEY,
-  name text UNIQUE NOT NULL,
-  population INTEGER NOT NULL
+-- CREATE TABLE IF NOT EXISTS cities (
+--   id serial PRIMARY KEY,
+--   name text UNIQUE NOT NULL,
+--   population INTEGER NOT NULL
+-- );
+
+-- INSERT INTO cities (name, population) VALUES
+--   ('Stockholm', 975904),
+--   ('Göteborg', 5836),
+--   ('Vänersborg', 347949),
+--   ('Malmö', 31251)
+--   ON CONFLICT (name) DO NOTHING;
+
+
+CREATE TABLE IF NOT EXISTS users (
+  user_id serial PRIMARY KEY,
+  name text NOT NULL,
+  mail text UNIQUE NOT NULL,
+  username text UNIQUE NOT NULL,
+  password text NOT NULL,
+  image_path text
 );
 
--- Lägg till tre svenska städer.
--- I och med att name är unique kommer inga dubletter att läggas till och inga felmeddelanden kommer att visas.
-INSERT INTO cities (name, population) VALUES
-  ('Stockholm', 975904),
-  ('Göteborg', 5836),
-  ('Vänersborg', 347949),
-  ('Malmö', 31251)
-  ON CONFLICT (name) DO NOTHING;
+CREATE TABLE IF NOT EXISTS topics (
+  topic_id serial PRIMARY KEY,
+  title text NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  created_by INTEGER NOT NULL,
+  FOREIGN KEY (created_by) REFERENCES users(user_id)
+);
 
+CREATE TABLE IF NOT EXISTS posts (
+  post_id serial PRIMARY KEY,
+  content text NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  created_by INTEGER NOT NULL,
+  topic INTEGER NOT NULL,
+  FOREIGN KEY (created_by) REFERENCES users(user_id),
+  FOREIGN KEY (topic) REFERENCES topics(topic_id)
+);
 
-  Users
-  ID: INT
-  Name: text
-  Username: text
-  Password: text
-  Mail: text
-  Image: ???
-  Role: text
+-- CREATE TABLE IF NOT EXISTS comments (
+--   comment_id serial PRIMARY KEY,
+--   content text NOT NULL,
+--   created_at TIMESTAMP NOT NULL DEFAULT NOW(),
+--   created_by INTEGER NOT NULL,
+--   post INTEGER NOT NULL,
+--   FOREIGN KEY (created_by) REFERENCES users(user_id),
+--   FOREIGN KEY (post) REFERENCES posts(post_id)
+-- )
 
-  Topics
-  ID: INT
-  Title: Text
-  CreatedBy: INT (User)
+CREATE TABLE IF NOT EXISTS userTopics (
+  user_id INTEGER NOT NULL,
+  topic_id INTEGER NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(user_id),
+  FOREIGN KEY (topic_id) REFERENCES topics(topic_id)
+);
 
-  Posts
-  ID: INT
-  Content: text
-  Time: Date
-  CreatedBy: INT (User)
-  Topic: INT (TopicID)
+INSERT INTO users (name, mail, username, password, image_path) VALUES (
+  'John Doe',
+  'johndoe@example.com',
+  'johndoe',
+  'password',
+  NULL
+);
 
-  -- Comments
-  -- ID: INT
-  -- Content: text
-  -- Time: Date
-  -- CreatedBy: INT (User)
-  -- Post: INT (PostID)
+INSERT INTO topics (title, created_by) VALUES (
+  ('Hello World Topic', 1),
+  ('Hello World Topic 2', 1),
+  ('Hello World Topic 3', 1)
+);
+
+INSERT INTO posts (content, created_by, topic) VALUES (
+  'Hello World Content!',
+  1,
+  1
+);
+
+-- INSERT INTO comments (content, created_by, post) VALUES (
+--   'Hello World!',
+--   1,
+--   1
+-- )
+
+INSERT INTO userTopics (user_id, topic_id) VALUES (
+  1,
+  1
+);
