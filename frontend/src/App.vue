d29f0868caed96af5e9141b69b37752aa4c6a9b3
Johan Hermansson <karljohan.hermanssons@gmail.com>
2023-11-15 11:42:32 +0100

    Skapade navbar och footer

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,22 @@
 <template>
-  <main class="bg-white">
-    <router-view></router-view>
+  <main class="bg-base-100">
+    <div class="bg-base-100 max-w-7xl mx-auto">
+      <Navbar />
+      <router-view class="my-12"></router-view>
+    </div>
+    <Footer />
   </main>
 </template>
 
 <script>
+import Navbar from './components/Navbar.vue';
+import Footer from './components/Footer.vue';
 export default {
   name: 'App',
+
+  components: {
+    Navbar,
+    Footer,
+  },
 };
 </script>
