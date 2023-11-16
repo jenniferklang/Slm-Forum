4fa6901b5c88bb978eef3f51b00c83de1c9653c2
Johan Hermansson <karljohan.hermanssons@gmail.com>
2023-11-16 23:19:51 +0100

    Småändringar för att få det att fungera på azure

--- backend/index.js
+++ backend/index.js
@@ -1,55 +1,36 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser')
-const http = require('http')
-const cors = require('cors')
-const { authenticateToken } = require('./middlewares/auth')
-const { initializeSocketIO } = require('./modules/socketio')
+const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const http = require('http');
+const cors = require('cors');
+const { authenticateToken } = require('./middlewares/auth');
+const { initializeSocketIO } = require('./modules/socketio');
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
-const app = express()
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.use(cookieParser())
-app.use(cors())
-app.use(authenticateToken)
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
+app.use(authenticateToken);
 
-const auth = require('./routes/auth')
-const api = require('./routes/api')
-const postTopics = require('./routes/postTopics')
-const forum = require('./routes/forum')
+const auth = require('./routes/auth');
+const api = require('./routes/api');
+const postTopics = require('./routes/postTopics');
+const forum = require('./routes/forum');
 
-app.use('/api/auth', auth)
-app.use('/api', api)
-app.use('/api/postTopics', postTopics)
-app.use('/api/forum', forum)
+app.use('/api/auth', auth);
+app.use('/api', api);
+app.use('/api/postTopics', postTopics);
+app.use('/api/forum', forum);
 
-const server = http.createServer(app)
-const io = initializeSocketIO(server)
+const server = http.createServer(app);
+const io = initializeSocketIO(server);
 io.on('error', (error) => {
-  console.error('Socket.IO Error:', error)
-})
+  console.error('Socket.IO Error:', error);
+});
 
 server.listen(port, () => {
-  console.log(`Ready on ${port}`)
-})
-
-// const io = socketIO(
-//   app.listen(port, () => {
-//     console.log(`Ready on ${port}`);
-//   })
-// );
-
-// io.on("connection", (socket) => {
-//   console.log("User connected");
-
-//   socket.on("disconnect", () => {
-//     console.log("User disconnected");
-//   });
-
-//   socket.on("chat message", (msg) => {
-//     console.log("Received message:", msg);
-//     io.emit("chat message", msg);
-//   });
-// });
+  console.log(`Ready on ${port}`);
+});
--- backend/modules/socketio.js
+++ backend/modules/socketio.js
@@ -1,40 +1,30 @@
-const { Server } = require('socket.io')
+const { Server } = require('socket.io');
 
 function initializeSocketIO(httpServer) {
   const io = new Server(httpServer, {
-    path: '/socketchat/',
+    path: '/socketchat', // /socketchat/
     cors: {
       // origin: 'http://frontend:80',
       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
       credentials: true,
     },
-  })
+  });
 
   io.on('connection', (socket) => {
-    console.log('User connected')
+    io.emit('new user', socket.id);
 
     socket.on('disconnect', () => {
-      console.log('User disconnected')
-    })
+      console.log('User disconnected');
+      io.emit('user disconnected', socket.id);
+    });
 
     socket.on('chat message', (msg) => {
-      console.log('Received message:', msg)
-      io.emit('chat message', msg)
-    })
-  })
+      console.log('Received message:', msg);
+      io.emit('chat message', msg);
+    });
+  });
 
-  // io.on('connection', (socket) => {
-  //   console.log('En klient anslöt.')
-  //   //När en klient skickar emittar ett meddelande med namnet 'chat message' så skickas det till alla anslutna klienter
-  //   socket.on('chat message', (msg) => {
-  //     io.emit('chat message', msg)
-  //   })
-  //   //När en klient ansluter så skickas ett meddelande till alla anslutna klienter
-  //   socket.on('disconnect', () => {
-  //     console.log('User disconnected')
-  //   })
-  // })
-  return io
+  return io;
 }
 
-module.exports = { initializeSocketIO }
+module.exports = { initializeSocketIO };
