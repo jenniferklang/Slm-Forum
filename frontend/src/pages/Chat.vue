969676f41a20794b04f0c236eaa7a2c07b2135a5
Johan Hermansson <karljohan.hermanssons@gmail.com>
2023-11-16 20:07:27 +0100

    Socket.io på klienten

--- frontend/src/pages/Chat.vue
+++ frontend/src/pages/Chat.vue
@@ -1,24 +1,26 @@
 <template>
   <div>
     <div
-      v-for="(msg, index) in messages"
-      :key="index"
-      :class="{
-        'chat-start': isOtherUser(msg.senderSocketId),
-        'chat-end': !isOtherUser(msg.senderSocketId),
-      }"
+      class="border-base-300 bg-base-100 rounded-b-box rounded-se-box flex min-h-[6rem] min-w-[18rem] max-w-4xl flex-wrap flex-col items-center justify-center gap-2 bg-top p-4"
     >
-      <div class="chat-header">
-        {{ msg.username }}
-        <time class="text-xs opacity-50">{{ getTimeAgo(msg.timestamp) }}</time>
-      </div>
+      <div
+        v-for="(msg, index) in messages"
+        :key="index"
+        :class="msg.username === loggedInUser ? 'chat-end' : 'chat-start'"
+      >
+        <div class="chat-header">
+          {{ msg.username }}
+          <time class="text-xs opacity-50">{{
+            getTimeAgo(msg.timestamp)
+          }}</time>
+        </div>
 
-      <div class="chat-bubble">{{ msg.message }}</div>
-      <div class="chat-footer opacity-50">
-        {{ msg.status }}
+        <div class="chat-bubble">{{ msg.message }}</div>
+        <div class="chat-footer opacity-50">
+          {{ msg.status }}
+        </div>
       </div>
     </div>
-
     <div class="flex space-x-2">
       <input
         v-model="message"
@@ -34,52 +36,80 @@
 </template>
 
 <script>
-import io from 'socket.io-client'
-import moment from 'moment'
+// import io from 'socket.io-client';
+import moment from 'moment';
+
+import { socket, state } from '../socket';
 
 export default {
   data() {
     return {
       socket: null,
       message: '',
-      messages: [],
       currentUserSocketId: '',
-    }
+    };
   },
 
   mounted() {
-    this.socket = io('http://localhost:3000', {
-      path: '/socketchat',
-    })
-    console.log('Component mounted')
-    this.socket.on('chat message', (msg) => {
-      console.log('Received message:', msg)
-      this.messages.push(msg)
-    })
+    console.log('Logged in user:', this.loggedInUser);
+    this.socket = socket;
+    // this.socket = io('http://localhost:3000', {
+    //   path: '/socketchat',
+    // });
+    // console.log('Socket ID:', this.socket);
+    // this.socket.on('connection', () => {
+    //   console.log('Socket connection:', socket);
+    // });
+    // this.socket.on('new user', (socketId) => {
+    //   console.log('New user connected:', socketId);
+    //   this.currentUserSocketId = socketId;
+    // });
+
+    this.socket.on('user disconnected', (socketId) => {
+      console.log('User disconnected:', socketId);
+    });
+  },
+
+  computed: {
+    loggedInUser() {
+      return sessionStorage.getItem('user_id');
+    },
+
+    connected() {
+      return state.connected;
+    },
+
+    users() {
+      return state.users;
+    },
+
+    messages() {
+      return state.messages;
+    },
   },
 
   methods: {
     isOtherUser(senderSocketId) {
-      return senderSocketId !== this.currentUserSocketId
+      return senderSocketId !== this.currentUserSocketId;
     },
 
     sendMessage() {
-      console.log('Sending message:', this.message)
+      console.log('Sending message:', this.message);
       if (this.message.trim() !== '') {
         this.socket.emit('chat message', {
           message: this.message,
-          username: 'Fast värde', // Replace with the actual username
+          username: sessionStorage.getItem('user_id'), // Replace with the actual username
           timestamp: new Date(), // Use the current timestamp
           status: 'Delivered', // Set the initial status
           senderSocketId: this.socket.id,
-        })
-        this.message = ''
+        });
+        this.message = '';
       }
     },
 
     getTimeAgo(timestamp) {
-      return moment(timestamp).fromNow() // Use Moment.js for time ago
+      return moment(timestamp).fromNow(); // Use Moment.js for time ago
     },
   },
-}
+};
 </script>
---
+++ frontend/src/socket.js
@@ -0,0 +1,34 @@
+import { reactive } from 'vue';
+import { io } from 'socket.io-client';
+
+export const state = reactive({
+  connected: false,
+  users: [],
+  messages: [],
+});
+
+export const socket = io('http://localhost:3000', {
+  path: '/socketchat',
+});
+
+socket.on('connect', () => {
+  state.connected = true;
+  console.log('Connected to socket.io server');
+});
+
+socket.on('disconnect', () => {
+  state.connected = false;
+});
+
+socket.on('new user', (args) => {
+  console.log('Other user connected to chat: ', args);
+});
+
+socket.on('chat message', (msg) => {
+  console.log('Received message:', msg);
+  state.messages.push(msg);
+});
+
+// socket.on('bar', (...args) => {
+//   state.barEvents.push(args);
+// });
